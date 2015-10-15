08760e8ad99c4a19383b058b1bdd6db6437cc8f8
Matheus Mendes <bigodines@gmail.com>
2015-10-15 14:29:02 -0700

    smarter constructor.

--- api_health/verifier.py
+++ api_health/verifier.py
@@ -5,7 +5,7 @@ import json
 class Verifier(object):
 
     def __init__(self, json_body):
-        self.json_obj = json.loads(json_body)
+        self.json_obj = json_body if isinstance(json_body, dict) else json.loads(json_body)
 
     def has_property(self, path):
         return path in self.json_obj
--- tests/test_verifier.py
+++ tests/test_verifier.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 import unittest
+import json
 from api_health.verifier import Verifier
 
 
 class JsonVerifier(unittest.TestCase):
 
+    def test_should_be_smart_about_constructor_initializtion(self):
+        simple_json = u'{ "foo": "bar" }'
+        json_dict = json.loads(simple_json)
+        try:
+            v1 = Verifier(simple_json)
+            v2 = Verifier(json_dict)
+        except:
+            self.fail('Verifier() constructor should deal with both ' \
+                    'string and object json')
+
     def test_should_check_for_json_property(self):
         simple_json = u'{ "foo": "bar" }'
         verifier = Verifier(simple_json)
         self.assertTrue(verifier.has_property('foo'))
+        self.assertFalse(verifier.has_property('bleh'))
