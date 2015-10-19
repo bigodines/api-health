231be8da444931e32f7fe8bdce94faa2809b3c1d
Matheus Mendes <bigodines@gmail.com>
2015-10-19 15:12:38 -0700

    implements JsonPath for lookup. Time to implement matching in more completx json objects.

--- api_health/verifier.py
+++ api_health/verifier.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
 
+from jsonpath_rw import parse
+
 
 class Verifier(object):
 
@@ -8,4 +10,5 @@ class Verifier(object):
         self.json_obj = json_body if isinstance(json_body, dict) else json.loads(json_body)
 
     def has_property(self, path):
-        return path in self.json_obj
+        expr = parse(path)
+        return len(expr.find(self.json_obj)) > 0
--- tests/test_verifier.py
+++ tests/test_verifier.py
@@ -5,19 +5,26 @@ from api_health.verifier import Verifier
 
 
 class JsonVerifier(unittest.TestCase):
-
-    def test_should_be_smart_about_constructor_initializtion(self):
+    def test_constructor_should_be_smart_about_params(self):
         simple_json = u'{ "foo": "bar" }'
         json_dict = json.loads(simple_json)
         try:
             v1 = Verifier(simple_json)
             v2 = Verifier(json_dict)
         except:
-            self.fail('Verifier() constructor should deal with both ' \
+            self.fail('Verifier() constructor should deal with both '
                     'string and object json')
+
+        self.assertTrue(v1.has_property('foo'))
+        self.assertTrue(v2.has_property('foo'))
 
     def test_should_check_for_json_property(self):
         simple_json = u'{ "foo": "bar" }'
         verifier = Verifier(simple_json)
         self.assertTrue(verifier.has_property('foo'))
         self.assertFalse(verifier.has_property('bleh'))
+
+    def test_should_check_arrays(self):
+        array_json = u'{ "foo": "bar", "baz": [ 1, 2, 3] }'
+        verifier = Verifier(array_json)
+        self.assertEquals(1, verifier.has_property("baz[1]"))
